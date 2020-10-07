3f9bc32f33993bd6abaf3ede1efa42440e0ee1bf
Muhammad Ishaq <zillsaqee@gmail.com>
2020-10-07 18:43:27 +0500

    converts app to PWA

---
+++ public_/y/precache-manifest.3ee204432b582c22fe91c6492d64ee0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "670374c57943bab5be01",
+    "url": "/static/css/main.8ae00399.chunk.css"
+  },
+  {
+    "revision": "670374c57943bab5be01",
+    "url": "/static/js/main.670374c5.chunk.js"
+  },
+  {
+    "revision": "84934fa4a52172d32837",
+    "url": "/static/js/1.84934fa4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12e3dac858061d088023b2bd48e2fa96",
+    "url": "/static/media/icon.12e3dac8.ico"
+  },
+  {
+    "revision": "01763ce71d3d2db8a2997f6171fbfc45",
+    "url": "/index.html"
+  }
+];
